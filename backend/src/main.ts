a0238385cfdaba9a66d3a5a30e3a9b4c85c43f30
Tomás L. Vidal <vidaltomas08@gmail.com>
2023-09-12 00:13:06 -0300

    Save State

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix("api");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //si envia datos que no estan en la lista valida tira error
+      whitelist:true,
+      forbidNonWhitelisted: true,
+      //
+      
+      // transforma los datos siempre que pueda, por si llega un string, lo transforam en number
+      transform: true
+    })
+  );
+  
   await app.listen(3000);
 }
 bootstrap();
