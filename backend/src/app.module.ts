9958e65169d86adc41178093cc2e51cc5b1e3aed
Tomás L. Vidal <vidaltomas08@gmail.com>
2023-09-12 17:59:20 -0300

    Setting Connection With Container DB + Set Seeder Module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CountriesModule } from './countries/countries.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SeederModule } from './seeds/seeder.module';
 
 @Module({
   imports: [
     CountriesModule,
+    SeederModule,
     TypeOrmModule.forRoot({
       type: 'mariadb',
-      host: '172.26.0.4',
+      host: 'search-countries-database-1',
       port: 3306,
       username: 'user',
       password: 'pass',
