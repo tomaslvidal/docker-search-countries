ba5183917c8e5ce2f1075ac265a34a1f41508123
Tomás L. Vidal <vidaltomas08@gmail.com>
2023-09-12 18:22:47 -0300

    Set Service Type GET, searchCountries
    - Retorna hasta 5 países
    - Recibe Parametro 'search'
    - El valor de la busqueda debe ser mayor o igual a 3 caracteres

--- backend/src/countries/countries.controller.ts
+++ backend/src/countries/countries.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { CountriesService } from './countries.service';
-import { CreateCountryDto } from './dto/create-country.dto';
-import { UpdateCountryDto } from './dto/update-country.dto';
 
 @Controller('countries')
 export class CountriesController {
-  constructor(private readonly countriesService: CountriesService) {}
-
-  @Post()
-  create(@Body() createCountryDto: CreateCountryDto) {
-    return this.countriesService.create(createCountryDto);
-  }
-
+  constructor(private readonly countriesService: CountriesService) { }
+  
   @Get()
-  findAll() {
-    return this.countriesService.findAll();
-  }
+  async searchCountries(@Query('search') search: string): Promise<any[]> {
+    if (search && search.length >= 3) {
+      const countries = await this.countriesService.searchCountries(search);
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.countriesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCountryDto: UpdateCountryDto) {
-    return this.countriesService.update(+id, updateCountryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.countriesService.remove(+id);
+      return countries;
+    }
+    else {
+      throw new HttpException('La consulta debe tener al menos 3 caracteres', HttpStatus.NO_CONTENT);
+    }
   }
 }
--- backend/src/countries/countries.service.ts
+++ backend/src/countries/countries.service.ts
@@ -1,34 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCountryDto } from './dto/create-country.dto';
-import { UpdateCountryDto } from './dto/update-country.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Country } from './entities/country.entity';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 
 @Injectable()
 export class CountriesService {
   constructor(
     @InjectRepository(Country)
     private readonly countryRepository: Repository<Country>
-  ){}
-  
-  create(createCountryDto: CreateCountryDto) {
-    return 'This action adds a new country';
-  }
+  ) { }
 
-  findAll() {
-    return this.countryRepository.find();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} country`;
-  }
-
-  update(id: number, updateCountryDto: UpdateCountryDto) {
-    return `This action updates a #${id} country`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} country`;
+  async searchCountries(search: string): Promise<Country[]> {
+    return await this.countryRepository.find({
+      where: [
+        { name: Like(`%${search}%`) },
+      ],
+      take: 5,
+    });
   }
 }
