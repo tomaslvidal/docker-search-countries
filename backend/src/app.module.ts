97bbeee7a12bc68cb2beefb18f89f7cc9ece2da2
Tomás L. Vidal <vidaltomas08@gmail.com>
2023-09-12 01:31:07 -0300

    Update Config MariaDB and Add Entity Country

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CountriesModule } from './countries/countries.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CountriesModule],
+  imports: [
+    CountriesModule,
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: '172.26.0.4',
+      port: 3306,
+      username: 'user',
+      password: 'pass',
+      database: 'proyect',
+      synchronize: true,
+      autoLoadEntities: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/countries/entities/country.entity.ts
+++ backend/src/countries/entities/country.entity.ts
@@ -1,1 +1,14 @@
-export class Country {}
+import { Entity, Column } from 'typeorm';
+
+@Entity()
+export class Country {
+    @Column({ primary: true, generated: true })
+    id: number;
+
+    @Column()
+    name: string;
+  
+    @Column()
+    population: number;
+}
+
