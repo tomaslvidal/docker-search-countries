607a2e3f9c24163659191d717eeb0684036bfff2
Tomás L. Vidal <vidaltomas08@gmail.com>
2023-09-11 23:58:11 -0300

    Update Res Countries

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CountriesModule } from './countries/countries.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CountriesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
